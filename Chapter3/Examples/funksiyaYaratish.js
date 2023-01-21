db6f41376992c2e10c4bc810c4c19a6710aad64f
abdurauf1996 <vakhobovabdurauf@gmail.com>
2023-01-22 02:35:29 +0900

    Chapter3 funksiyalar funksiyaYaratish

---
+++ Chapter3/Examples/funksiyaYaratish.js
@@ -0,0 +1,20 @@
+function findY(x){
+    return x-2;
+}
+
+function findS(t, d){
+    return d/t
+}
+
+let y = findY(10); //Javobi 8
+console.log(y);
+
+let s = findS(3, 165); //Javobi 55
+console.log(s);
+
+function findAge(tugilganYil){
+    return 2023 - tugilganYil;
+}
+
+let age = findAge(2001); //Javobi 27
+console.log(age); 
