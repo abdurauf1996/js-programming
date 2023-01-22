b674b24fd63357548036cc5f235f8d8c6756bd97
abdurauf1996 <vakhobovabdurauf@gmail.com>
2023-01-22 19:23:47 +0900

    homwork

---
+++ Chapter3/Examples/2.js
@@ -0,0 +1,8 @@
+function yoshTopish(tugilganYil) {
+   return 2024-tugilganYil
+}
+let age = yoshTopish(2001);
+ console.log(age);
+
+
+ 
---
+++ Chapter3/Examples/y.js
@@ -0,0 +1,25 @@
+// y(x, z) = z + x + 1
+// x = 2
+// z = 1
+//y?
+function findY(x, z){
+    return z+x+1
+}
+let keepFind = findY(5,3) // 1.findY ni chaqrish uchun keepFindga saqlab oldik
+console.log(keepFind);// 2.saqlangan funksiyani keepFind orqali chairdik
+
+
+
+function findAge(tugilganYil){
+    return 2023-tugilganYil
+}
+let age = findAge(1996);
+ console.log(age); 
+
+ function topishD(v=10,h=12){
+    return v+h+12
+ }
+let d = topishD(100,200)
+console.log(d);
+
+
