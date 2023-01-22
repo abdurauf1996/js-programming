d83100e1bcc6a8376eddd950f9a1fd93b01a64da
abdurauf1996 <vakhobovabdurauf@gmail.com>
2023-01-22 23:19:50 +0900

    Quiz 1mashq

--- Chapter3/Examples/2.js
+++ Chapter3/Examples/2.js
@@ -5,4 +5,41 @@ let age = yoshTopish(2001);
  console.log(age);
 
 
- 
+ function qidrishL(x,d){
+    return x-d
+ }
+ let L = qidrishL(21,15)
+ console.log(L);
+
+
+ function yoshTopish( tugilganYil){
+    return 2023-tugilganYil
+ }
+ let logi = yoshTopish (1969)
+ console.log( logi);
+  
+ function yosh(dadam,onam){
+    return dadam-onam
+ }
+let farqi = yosh (1969,1974)
+console.log(farqi);
+
+
+function yoshi(x){
+    return 2023-x
+}
+let lov = yoshi (1926)
+console.log(lov);
+
+function los(x){
+    return 2023-x
+}
+let m = los(17)
+console.log(m);
+
+
+
+
+
+
+
