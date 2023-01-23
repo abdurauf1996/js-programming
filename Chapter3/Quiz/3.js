95435ffecac17e7b6f658ddebc3c38e77cae7d4d
abdurauf1996 <vakhobovabdurauf@gmail.com>
2023-01-23 18:15:58 +0900

    Quiz Homework 3 mashq

--- Chapter3/Quiz/3.js
+++ Chapter3/Quiz/3.js
@@ -1,4 +1,16 @@
 function printX(username){
-    console.log("Username ;",username)
-    // 
+    console.log("Username ;",username)   
 }
+/* BU yerda  "function" bu funksiyani kalit so"zi bolib funkisya boshlanyotganini bildiradi
+"printX" esa funksiya nomi funkisayni qiymatni qaytarda
+"username" parametr listi yani funksiyani boshlanishi
+"{}" funksiyani boshlovi va tugatuchi
+"return"  kalit soz  chaqrilgan joyga qiymat  qaytrish uchun ishlatiladi
+"console.log" terminalga togri yoki xatoligi tekshrish uchun ishaltiladi
+
+
+
+
+
+
+
