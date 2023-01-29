de68296f14a2c7ec65e08da62803ec53f3e00510
abdurauf1996 <vakhobovabdurauf@gmail.com>
2023-01-29 16:28:06 +0900

    Quiz2 2.js

---
+++ Chapter3/Quiz2/2.js
@@ -0,0 +1,5 @@
+function aylanaY(r,){
+    return 3.14*(r*2);
+}
+const aylanP = aylanaY(4);
+console.log(aylanP);
---
+++ Chapter3/Quiz2/3.js
@@ -0,0 +1,5 @@
+function pifagorC(a,b){
+    return Math.sqrt(a*a)+(b*b)
+}
+let pif = pifagorC(2,3);
+console.log(pif);
