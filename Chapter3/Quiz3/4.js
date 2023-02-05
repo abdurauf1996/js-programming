90b2b4efbb04be2f1ad370b87a297d9d207b81c6
abdurauf1996 <vakhobovabdurauf@gmail.com>
2023-02-05 20:39:16 +0900

    Quiz3 4.js

---
+++ Chapter3/Quiz3/4.js
@@ -0,0 +1,17 @@
+let x = true;  // bu yerda 2ta operaturani bir birga tenglayabmiz budna javobmiz true;
+let y = false;
+let z = x == x;
+// z = true
+
+// let x = true;
+// let y = false;
+// let z = x == y; budna javobmiz "false" chiqadi  
+ 
+// let x = true;
+// let y = false;
+// let z = y == y;
+//  budna z= true
+
+// let x = true;
+// let y = false;
+// let z = x === y; bunda xam false boladi 
