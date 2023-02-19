7384965f44e0cf7ac1b666da18a9d8069d5bad0f
abdurauf1996 <vakhobovabdurauf@gmail.com>
2023-02-19 15:49:45 +0900

    Chapter3 Quiz4 1.js

---
+++ Chapter3/Quiz4/1.js
@@ -0,0 +1,8 @@
+let x = 10;
+let y = "10";
+let z = 10;
+                     
+let salomlashuv = ((y === x || x == y ? x : y) === z ? "Solla yaxshimi?" : "What's up?") //'Solla yaxshimi?'
+
+let salomlashuv2 = ((y === x && x == y ? x : y) === z ? "Solla yaxshimi?" : "What's up?") // "what's up?"
+ 
