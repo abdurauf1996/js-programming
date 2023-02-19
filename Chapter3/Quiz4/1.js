2240bc3022dd74fe6fbaf8a74eb691cb4f7420f2
abdurauf1996 <vakhobovabdurauf@gmail.com>
2023-02-19 17:09:07 +0900

    Chapter3 2.js

--- Chapter3/Quiz4/1.js
+++ Chapter3/Quiz4/1.js
@@ -5,4 +5,5 @@ let z = 10;
 let salomlashuv = ((y === x || x == y ? x : y) === z ? "Solla yaxshimi?" : "What's up?") //'Solla yaxshimi?'
 
 let salomlashuv2 = ((y === x && x == y ? x : y) === z ? "Solla yaxshimi?" : "What's up?") // "what's up?"
- 
+
+console.log(salomlashuv, salomlashuv2);
---
+++ Chapter3/Quiz4/2.js
@@ -0,0 +1,6 @@
+let x = 15
+let v = 33
+let  solishtruv = (x == v)  ? "togri" : "togriemas"
+console.log(solishtruv);
+
+
