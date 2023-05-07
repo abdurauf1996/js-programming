963302a0475f3b1805d07c9e8d4214b03fa11435
abdurauf1996 <vakhobovabdurauf@gmail.com>
2023-05-07 16:40:42 +0900

    mashiq

--- Chapter1/firstCode.js
+++ Chapter1/firstCode.js
@@ -1,11 +1,13 @@
-console.log("Hello, World!")
+console.log("Hello, World!");
 let x = 25;
 let y = 100;
-let sum = x + y
+let sum = x + y;
 console.log(sum);
 
-console.log( "Sejong, unversity") 
-let a = 12;
-let b =13;
-let xisob = a + b
-console.log(xisob);
+console.log("Sejong, unversity");
+
+function men(a, b) {
+  return a + b;
+}
+let son = men(34, 56);
+console.log(son);
